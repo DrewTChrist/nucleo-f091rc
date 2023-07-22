dd8f45ce004253a6c416ea0ac6d4fa3884bc85ff
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2023-07-22 09:09:08 -0700

    Update airlift example to use esp32-wroom-rp

--- src/bin/airlift.rs
+++ src/bin/airlift.rs
@@ -1,12 +1,11 @@
 #![no_main]
 #![no_std]
 
+use esp32_wroom_rp::{gpio::EspControlPins, wifi::Wifi};
 //use panic_halt as _;
 use defmt::*;
 use defmt_rtt as _;
 use panic_probe as _;
-
-use wifi_nina;
 
 use hal::{
     delay::Delay,
@@ -25,12 +24,13 @@ use core::{cell::RefCell, ops::DerefMut};
 static INTERRUPT: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
 
 const MODE: Mode = Mode {
-    polarity: Polarity::IdleHigh,
-    phase: Phase::CaptureOnSecondTransition,
+    polarity: Polarity::IdleLow,
+    phase: Phase::CaptureOnFirstTransition,
 };
 
 #[entry]
 fn main() -> ! {
+    defmt::println!("Program starting");
     let mut peripherals = pac::Peripherals::take().unwrap();
     let rcc = peripherals.RCC;
     rcc.apb2enr.modify(|_, w| w.syscfgen().set_bit());
@@ -49,9 +49,6 @@ fn main() -> ! {
     let mut led = cortex_m::interrupt::free(|cs| gpioa.pa5.into_push_pull_output(cs));
     led.toggle().ok();
     let _user_button = cortex_m::interrupt::free(|cs| gpioc.pc13.into_pull_down_input(cs));
-    let busy = cortex_m::interrupt::free(|cs| gpiob.pb10.into_floating_input(cs));
-    let reset = cortex_m::interrupt::free(|cs| gpioa.pa8.into_push_pull_output(cs));
-    let chip_select = cortex_m::interrupt::free(|cs| gpioa.pa10.into_push_pull_output(cs));
     let (sck, miso, mosi) = cortex_m::interrupt::free(|cs| {
         (
             gpiob.pb3.into_alternate_af0(cs),
@@ -63,20 +60,20 @@ fn main() -> ! {
         peripherals.SPI1,
         (sck, miso, mosi),
         MODE,
-        100_000.hz(),
+        8_000_000.hz(),
         &mut rcc,
     );
-    let delay = Delay::new(cp.SYST, &rcc);
-    let wifi_delay = |d: core::time::Duration| {
-        delay.delay_ms(d.as_millis() as u32);
+    let mut delay = Delay::new(cp.SYST, &rcc);
+    let esp_pins = EspControlPins {
+        cs: cortex_m::interrupt::free(|cs| gpioa.pa10.into_push_pull_output(cs)),
+        gpio0: cortex_m::interrupt::free(|cs| gpioa.pa2.into_push_pull_output(cs)),
+        resetn: cortex_m::interrupt::free(|cs| gpioa.pa8.into_push_pull_output(cs)),
+        ack: cortex_m::interrupt::free(|cs| gpiob.pb10.into_floating_input(cs)),
     };
-    let wifi = wifi_nina::Wifi::new(wifi_nina::transport::SpiTransport::start(
-        spi,
-        busy,
-        reset,
-        chip_select,
-        wifi_delay
-    ).unwrap());
+
+    let mut wifi = Wifi::init(spi, esp_pins, &mut delay).unwrap();
+    let version = wifi.firmware_version();
+    defmt::println!("{}", version);
     syscfg.exticr4.modify(|_, w| w.exti13().pc13());
 
     // Set interrupt request mask for line 13
@@ -102,6 +99,7 @@ fn main() -> ! {
 
 #[interrupt]
 fn EXTI4_15() {
+    defmt::println!("INTERRUPT");
     cortex_m::interrupt::free(move |cs| {
         // Clear interrupt
         if let Some(exti) = INTERRUPT.borrow(cs).borrow_mut().deref_mut() {
